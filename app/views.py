06b49238f87837891c539e4391003f2c1d27b5e7
chrajeswari <ch.raji@32003@gmail.com>
2023-11-10 19:53:08 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def chinnu(request):
+    return render(request,'chinnu.html')
+def one(request):
+    return render(request,'one.html')
+def two(request):
+    return render(request,'two.html')
+def three(request):
+    return render(request,'three.html')
